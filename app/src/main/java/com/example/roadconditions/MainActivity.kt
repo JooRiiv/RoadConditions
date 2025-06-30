5430b9c7960a7d495efc9dbf8cd8b3e8b61811d6
Joonas Riivari <jonttis@gmail.com>
2025-06-30 15:43:16 +0300

    feat: add mock data

--- app/src/main/java/com/example/roadconditions/MainActivity.kt
+++ app/src/main/java/com/example/roadconditions/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var googleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,7 +25,34 @@ class MainActivity : AppCompatActivity() {
             insets
 
         }
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
+
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        addCustomMarker()
+    }
+
+
+    private fun addCustomMarker() {
+        // Mock data
+        val locations = listOf(LatLng(61.462087, 23.843748),
+        LatLng(61.465286, 23.812305),
+        LatLng(61.471189, 23.861733)
+        )
+        for (location in locations) {
+            val markerOptions = MarkerOptions()
+                .position(location)
+                .title("Possible Bump Detected")
+                .snippet("Testi")
+            googleMap.addMarker(markerOptions)
+        }
+        val tampere = LatLng(61.497234,23.759126)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tampere, 12f))
     }
 
 }
